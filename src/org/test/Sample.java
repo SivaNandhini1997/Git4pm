f7f62b9563d3bd596b8fd08a4af2a254d30ebf9c
SivaNandhini1997 <nandynandhukannan@gmail.com>
2022-08-29 17:01:01 +0530

    Lines of code added

---
+++ src/org/test/Sample.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Siva.......");
+		
+		System.out.println("Successfull........");
+		
+	}
+
+}
+ 
